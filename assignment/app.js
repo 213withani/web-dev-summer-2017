ee1555d057befa4cb8af9af7501ea33fb598c012
Israel <213withani@gmail.com>
2017-10-23 22:50:03 -0700

    refactored

--- assignment/app.js
+++ assignment/app.js
@@ -5,19 +5,18 @@ var users = [
     { _id: 234, username: "bob", password: "bob", firstName: "Bob", lastName: "Bob" },
 ];
 
-app.get('/users', getAllUsers)
-
-app.get('/user/:userId', getUserById)
+// api token meant to return dynamic data, 
+// user won't create directory api and adding files there causing confuscius
+app.get('/api/users', getAllUsers)
+app.get('/api/user/:userId', getUserById)
 
 function getUserById(req, res) {
     for (var u in users) {
 
         if (users[u]._id == req.params.userId) {
-            res.send(users[u])
-
+            res.send(users[u]) 
         }
     }
-
 }
 
 function getAllUsers(req, res) {
