f7de70da19c0feed479be31c8374ea7a25f03d5f
Israel <213withani@gmail.com>
2017-10-28 18:32:57 -0700

    Moving website client code to server

---
+++ assignment/website.service.server.js
@@ -0,0 +1,20 @@
+var app = require("../express");
+
+app.get("/api/user/:userId/website", findWebsitesForUser);
+
+var websites = [
+    { "id": "123", "name": "Facebook", "developerId": "123", "description": "Yo facebook" },
+    { "id": "234", "name": "Tweeter", "developerId": "456", "description": "Yo Tweet" }
+];
+
+function findWebsitesForUser(req, res) {
+    var userId = req.params.userId;
+    var sites = [];
+
+    for (var w in websites) {
+        if (websites[w].developerId === userId) {
+            sites.push(websites[w]);
+        }
+    }
+    res.send(sites);
+}
--- public/assignment/services/websiteService.client.js
+++ public/assignment/services/websiteService.client.js
@@ -4,22 +4,11 @@
         .service("websiteService", websiteService);
 
     function websiteService() {
-        var websites = [
-            { "id": "123", "name": "Facebook", "developerId": "123", "description": "Yo facebook" },
-            { "id": "234", "name": "Tweeter", "developerId": "456", "description": "Yo Tweet" }
-        ];
-
         this.findWebsitesForUser = findWebsitesForUser;
 
         function findWebsitesForUser(userId) {
-            var sites = [];
-
-            for (var w in websites) {
-                if (websites[w].developerId === userId) {
-                    sites.push(websites[w]); 
-                }
-            }
-            return sites;
+            var url = "/api/user/" + userId + "/website";
+            $http.get(url);
         }
     }
 })();
